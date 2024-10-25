ef3f809e0795241954d4bb03d33c9deaabd753fa
JohnSiiger <jonatan.siiger@gmail.com>
2024-10-25 11:43:19 +0200

    Update Triangle.h

--- src/Triangle.h
+++ src/Triangle.h
@@ -1,1 +1,19 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+using namespace std;
 
+class Triangle(){
+  public:
+    Triangle();
+    Triangle(double a, double b, double c);
+    void setSides(doubla a, double b, double c);
+    double circumference();
+    double area();
+    void print()
+  private:
+    double a;
+    double b;
+    double c;
+};
+
+#endif
