a23729e3545c98a49b1fe711238f8eb4e654af07
JohnSiiger <jonatan.siiger@gmail.com>
2024-10-25 11:57:49 +0200

    Update Triangle.h

--- src/Triangle.h
+++ src/Triangle.h
@@ -4,13 +4,24 @@ using namespace std;
 
 class Triangle(){
   public:
+  // Constructors
     Triangle();
     Triangle(double a, double b, double c);
-    void setSides(doubla a, double b, double c);
+  // Functions
+    void setSides(double a, double b, double c);
     double circumference();
     double area();
     void print();
+  // Getters
+    double getA();
+    double getB();
+    double getC();
+  // Setters
+    void setA();
+    void setB();
+    void setC();
   private:
+  // Data members
     double a;
     double b;
     double c;
