2c6dde9949fc259c0ccb9393b15ce76f8e2dfdda
JohnSiiger <jonatan.siiger@gmail.com>
2024-10-25 12:32:06 +0200

    Update Triangle.cpp

--- src/Triangle.cpp
+++ src/Triangle.cpp
@@ -1,1 +1,24 @@
-
+#include "Triangle.h"
+#include <cmath>
+Triangle::Triangle(){
+  a = 0;
+  b = 0;
+  c = 0;
+}
+Triangle::Triangle(double a, double b, double c) : a(a), b(b), c(c) {}
+void Triangle::setSides(double a, double b, double c){
+  this->a = a;
+  this->b = b;
+  this->c = c;
+}
+double Triangle::circumference(){
+  return a + b + c;
+}
+double Triangle::area(){
+  double s = (a + b + c)/2;
+  double A = sqrt(s*(s-a)*(s-b)*(s-c));
+  return A;
+}
+void Triangle::print(){
+  cout << "A is the area of the triangle: " << area() <<  endl;
+}
